42c1a21e08466fed630308e127deeb8335a38b03
taewon <ytw9699>
2023-08-22 12:07:10 +0900

    fix : 구독채널명 변경

--- chatServer/src/main/java/com/example/chatserver/config/RedisConfig.java
+++ chatServer/src/main/java/com/example/chatserver/config/RedisConfig.java
@@ -40,7 +40,7 @@ public class RedisConfig {
     @Bean
     RedisMessageListenerContainer redisContainer(@Qualifier("chatListenerAdapter") MessageListenerAdapter chatListenerAdapter) {//컨테이너 설정
 
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
                                       container.setConnectionFactory(redisConnectionFactory());
                                       container.addMessageListener(chatListenerAdapter, chatTopic());//구독한다
         return container;
@@ -48,6 +48,6 @@ public class RedisConfig {
 
     @Bean
     ChannelTopic chatTopic() {  //pub/sub 토픽 설정
-        return new ChannelTopic("chatSend");
+        return new ChannelTopic("chatTopic");
     }
 }
--- chatServer/src/main/java/com/example/chatserver/service/RedisPubService.java
+++ chatServer/src/main/java/com/example/chatserver/service/RedisPubService.java
@@ -3,6 +3,7 @@ package com.example.chatserver.service;
 import com.example.chatserver.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +11,10 @@ import org.springframework.stereotype.Service;
 public class RedisPubService {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ChannelTopic chatTopic;
 
     public void sendChatMessage(ChatMessage chatMessage) {
         System.out.println("퍼블리시 하는 메시지 = " + chatMessage);
-        redisTemplate.convertAndSend("chatSend", chatMessage);
+        redisTemplate.convertAndSend(chatTopic.getTopic(), chatMessage);//chatTopic
     }
 }
